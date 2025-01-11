c0115d96f503dea189eaeb71fecf5806b1d035bd
Rasmus <mail@rasmusj.dk>
2025-01-11 23:57:51 +0100

    try again

--- src/scripts/get_updated_sources.sh
+++ src/scripts/get_updated_sources.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 ITEMS=""
 for item in "${PARAM_UPD_SRC_FILES}"/*; do
-    if [ -e "$item" ]; then
-        ITEMS="${ITEMS} $(basename "$item")"
-    fi
+        if [ -e "$item" ]; then
+                ITEMS="${ITEMS} $(basename "$item")"
+        fi
 done
 echo "Items in $PARAM_UPD_SRC_FILES: $ITEMS"
 if [ -z "$ITEMS" ]; then
-    echo "No updated sources found"
-    circleci-agent step halt # Stop the job
+        echo "No updated sources found"
+        circleci-agent step halt # Stop the job
 else
-    echo "Updated sources found. Copying files to the repository"
-    cp -a "${PARAM_UPD_SRC_FILES}/." .
+        echo "Updated sources found. Copying files to the repository"
+        cp -a "${PARAM_UPD_SRC_FILES}/." .
 fi
