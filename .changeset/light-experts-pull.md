6f096b5d3d4e179764c520ebd52d6ca7e45cf5a3
Rasmus <67839982+RelativeSure@users.noreply.github.com>
2025-02-25 00:58:41 +0100

    Deprecate git feature (#36)
    * Delete src/commands/run_git.yml
    
    * Update run.yml
    
    * Delete src/scripts/check_updated_sources.sh
    
    * Delete src/scripts/debug.sh
    
    * Delete src/scripts/setup_git.sh
    
    * Update run_megalinter.yml
    
    * Update @orb.yml
    
    * Update test-deploy.yml
    
    * Update @orb.yml
    
    * Update config.yml
    
    * Create light-experts-pull.md
    
    * Update test-deploy.yml
    
    * Update test-deploy.yml
    
    * Update test-deploy.yml
    
    ---------
    
    Co-authored-by: KubeArchitectBot <139585163+KubeArchitectBot@users.noreply.github.com>

---
+++ .changeset/light-experts-pull.md
@@ -0,0 +1,5 @@
+---
+"@fake-scope/fake-pkg": patch
+---
+
+Deprecate git feature
--- src/scripts/check_updated_sources.sh
+++
@@ -1,4 +0,0 @@
-if [ ! -d "$PARAM_UPD_SRC_FILES" ]; then
-        echo "$PARAM_UPD_SRC_FILES does not exist. Stopping job..."
-        circleci-agent step halt # Stop the job
-fi
